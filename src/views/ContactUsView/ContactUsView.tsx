5c57e6846341febc18708e38d47b3852c6fe978c
happimaker <gregdev1023@gmail.com>
2022-01-18 11:51:54 -0800

    update contact form inputs using state

--- src/views/ContactUsView/ContactUsView.tsx
+++ src/views/ContactUsView/ContactUsView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { FC } from 'react';
 import axios from 'axios';
 import { backendUrl } from 'src/config';
@@ -17,16 +17,28 @@ const handleSubmit = async (event: React.SyntheticEvent, data: Data) => {
   event.preventDefault();
 
   try {
-    await axios.post(`${backendUrl}`, {email: 'jefferylin35@hotmail.com', subject: 'skills-react Submission', body: 'test'});
+    await axios.post(`${backendUrl}`, data);
   } catch (err) {
     console.error(err);
   }
 };
 
 const ContactUsView: FC<ContactUsViewProps> = ({ onSubmit = handleSubmit }) => {
+  const [data, setData] = useState({
+    email: '',
+    subject: 'skills-react Submission',
+    body: ''
+  });
+
+  const handleChange = (field: string, value: string) => {
+    setData((prev) => ({
+      ...prev,
+      [field]: value
+    }));
+  };
 
   return (
-    <form onSubmit={(e) => onSubmit(e, {})}>
+    <form onSubmit={(e) => onSubmit(e, data)}>
       <div className="mb-3">
         <label htmlFor="email" className="form-label">Email</label>
         <input
@@ -35,6 +47,8 @@ const ContactUsView: FC<ContactUsViewProps> = ({ onSubmit = handleSubmit }) => {
           className="form-control"
           id="email"
           aria-describedby="emailHelp"
+          value={data.email}
+          onChange={(e) => handleChange('email', e.target.value)}
         />
       </div>
       <div className="mb-3">
@@ -45,12 +59,18 @@ const ContactUsView: FC<ContactUsViewProps> = ({ onSubmit = handleSubmit }) => {
           name="subject"
           id="subject"
           aria-describedby="subject"
-          value="skills-react Submission"
+          value={data.subject}
           disabled
         />
       </div>
       <div className="mb-3 form-floating">
-        <textarea className="form-control" placeholder="Leave a comment here" id="body" />
+        <textarea
+          className="form-control"
+          placeholder="Leave a comment here"
+          id="body"
+          value={data.body}
+          onChange={(e) => handleChange('body', e.target.value)}
+        />
         <label htmlFor="body">Body</label>
       </div>
       <button className="btn btn-primary">Submit</button>
